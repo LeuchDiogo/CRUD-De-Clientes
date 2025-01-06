ac41cded8c11773c776ebe676d1b3d865117670d
LeuchDiogo <diogo14leuch@gmail.com>
2025-01-06 17:01:36 -0300

    Get Clients

--- src/main/java/com/Leuch/CrudDeClientes/Controller/ClientController.java
+++ src/main/java/com/Leuch/CrudDeClientes/Controller/ClientController.java
@@ -4,6 +4,8 @@ package com.Leuch.CrudDeClientes.Controller;
 import com.Leuch.CrudDeClientes.dto.ClientDTO;
 import com.Leuch.CrudDeClientes.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +23,13 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id)
     {
         ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable)
+    {
+        Page<ClientDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 
--- src/main/java/com/Leuch/CrudDeClientes/services/ClientService.java
+++ src/main/java/com/Leuch/CrudDeClientes/services/ClientService.java
@@ -4,6 +4,9 @@ import com.Leuch.CrudDeClientes.Entities.Client;
 import com.Leuch.CrudDeClientes.dto.ClientDTO;
 import com.Leuch.CrudDeClientes.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +23,12 @@ public class ClientService {
         Optional<Client> result = repository.findById(id);
         Client client = result.get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 
 }
