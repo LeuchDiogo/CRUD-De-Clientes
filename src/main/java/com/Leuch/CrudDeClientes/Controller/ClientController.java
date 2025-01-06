52b3d35d8d29cb897028d19850af17789909a5a4
LeuchDiogo <diogo14leuch@gmail.com>
2025-01-06 16:44:57 -0300

    ClientService, ClientController, Get client By id

--- src/main/java/com/Leuch/CrudDeClientes/Controller/ClientController.java
+++ src/main/java/com/Leuch/CrudDeClientes/Controller/ClientController.java
@@ -1,4 +1,27 @@
 package com.Leuch.CrudDeClientes.Controller;
 
+
+import com.Leuch.CrudDeClientes.dto.ClientDTO;
+import com.Leuch.CrudDeClientes.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
 public class ClientController {
+
+    @Autowired
+    ClientService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id)
+    {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
 }
--- src/main/java/com/Leuch/CrudDeClientes/services/ClientService.java
+++ src/main/java/com/Leuch/CrudDeClientes/services/ClientService.java
@@ -1,14 +1,25 @@
 package com.Leuch.CrudDeClientes.services;
 
+import com.Leuch.CrudDeClientes.Entities.Client;
+import com.Leuch.CrudDeClientes.dto.ClientDTO;
 import com.Leuch.CrudDeClientes.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
     @Autowired
     ClientRepository repository;
 
-
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id)
+    {
+        Optional<Client> result = repository.findById(id);
+        Client client = result.get();
+        return new ClientDTO(client);
+    }
 
 }
