3566ad51d7e2228288759676d93fb117d17589d2
LeuchDiogo <diogo14leuch@gmail.com>
2025-01-06 15:49:38 -0300

    Star Project

---
+++ src/test/java/com/Leuch/CrudDeClientes/CrudDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Leuch.CrudDeClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
