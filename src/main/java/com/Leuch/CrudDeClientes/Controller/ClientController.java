2dd23a597121e40fb5fff6021515e8f50c5c4c98
LeuchDiogo <diogo14leuch@gmail.com>
2025-01-07 14:17:39 -0300

    POST Client, PUT Client

--- src/main/java/com/Leuch/CrudDeClientes/Controller/ClientController.java
+++ src/main/java/com/Leuch/CrudDeClientes/Controller/ClientController.java
@@ -1,16 +1,17 @@
 package com.Leuch.CrudDeClientes.Controller;
 
 
+import com.Leuch.CrudDeClientes.Entities.Client;
 import com.Leuch.CrudDeClientes.dto.ClientDTO;
 import com.Leuch.CrudDeClientes.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -32,5 +33,21 @@ public class ClientController {
         Page<ClientDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+        dto  = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update (@PathVariable Long id, @RequestBody ClientDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+
 
 }
